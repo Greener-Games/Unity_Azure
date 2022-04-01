cfe0a6379217721b380e56237bf6fd5981763c83
Reddevildragg <Thomas.greener@hotmail.co.uk>
2022-04-01 17:33:09 +0100

    better control over upload files

--- Packages/Atkins Azure Helpers/Scripts/Storage/AzureFileStorage.cs
+++ Packages/Atkins Azure Helpers/Scripts/Storage/AzureFileStorage.cs
@@ -86,11 +86,11 @@ namespace Atkins.AzureHelpers
             return cloudBlob;
         }
 
-        public static async Task<bool> UploadFile(string containerName, string sourceFile, string blobSaveLocation, CancellationToken token,
+        public static async Task<bool> UploadFile(string containerName, string sourceFile, string blobLocation, CancellationToken token,
                                                   BlobType blobType = BlobType.PAGE, Action<ProgressRecorder> processCallback = null, int tries = 0, BlobServiceClient client = null)
         {
             BlobContainerClient container = await GetContainer(containerName, client);
-            return await UploadFile(container, sourceFile, blobSaveLocation, token, blobType, processCallback, tries);
+            return await UploadFile(container, sourceFile, blobLocation, token, blobType, processCallback, tries);
         }
 
         /// <summary>
@@ -98,13 +98,13 @@ namespace Atkins.AzureHelpers
         /// </summary>
         /// <param name="container">The Blob Container</param>
         /// <param name="sourceFile">The path of the file to upload</param>
-        /// <param name="blobSaveLocation">The blob name</param>
+        /// <param name="blobLocation">The blob name</param>
         /// <param name="token">The cancellation token. Used only in TransferManager.UploadAsync method.</param>
         /// <param name="blobType">The type of the blob. By default: PAGE</param>
         /// <param name="processCallback">The progress callback element. Used for creating the progress recorder</param>
         /// <param name="tries"></param>
         /// <returns></returns>
-        public static async Task<bool> UploadFile(BlobContainerClient container, string sourceFile, string blobSaveLocation, CancellationToken token, BlobType blobType = BlobType.PAGE, Action<ProgressRecorder> processCallback = null, int tries = 0)
+        public static async Task<bool> UploadFile(BlobContainerClient container, string sourceFile, string blobLocation, CancellationToken token, BlobType blobType = BlobType.PAGE, Action<ProgressRecorder> processCallback = null, int tries = 0)
         {
             try
             {
@@ -112,14 +112,14 @@ namespace Atkins.AzureHelpers
 
                 BlobBaseClient cloudBlob = blobType switch
                 {
-                    BlobType.PAGE => container.GetPageBlobClient(blobSaveLocation),
-                    BlobType.APPEND => container.GetAppendBlobClient(blobSaveLocation),
-                    BlobType.BLOCK => container.GetBlockBlobClient(blobSaveLocation),
+                    BlobType.PAGE => container.GetPageBlobClient(blobLocation),
+                    BlobType.APPEND => container.GetAppendBlobClient(blobLocation),
+                    BlobType.BLOCK => container.GetBlockBlobClient(blobLocation),
                     _ => throw new ArgumentOutOfRangeException(nameof(blobType), blobType, null)
                 };
 
                 Debug.Log("Container :" + cloudBlob);
-                Debug.Log($"Uploading file {sourceFile} to {blobSaveLocation}");
+                Debug.Log($"Uploading file {sourceFile} to {blobLocation}");
 
                 FileInfo fileInfo = new FileInfo(sourceFile);
                 long uploadFileSize = fileInfo.Length;
@@ -165,30 +165,31 @@ namespace Atkins.AzureHelpers
         /// Download a file from Azure server async
         /// </summary>
         /// <param name="containerName">The name of the CloudBlobContainer</param>
-        /// <param name="sourceLocation">The blob name</param>
-        /// <param name="saveLocation">The path to the file we want to delete</param>
+        /// <param name="sourceFile">The blob name</param>
+        /// <param name="saveFile">The path to the file we want to delete</param>
         /// <param name="processCallback">The progress callback</param>
         /// <param name="maxTries">Maximum numbers of tried before failing</param>
         /// <param name="currentTries">current number of tried for the download</param>
         /// <returns></returns>
-        public static async Task<bool> DownloadFile(string containerName,string sourceLocation, string saveLocation,
+        public static async Task<bool> DownloadFile(string containerName,string sourceFile, string saveFile, bool appendFilePath = true, 
                                                     Action<ProgressRecorder> processCallback = null, int maxTries = 3, int currentTries = 0, BlobServiceClient client = null)
         {
             BlobContainerClient container = await GetContainer(containerName, client);
-            return await DownloadFile(container, sourceLocation, saveLocation, processCallback, maxTries, currentTries);
+            return await DownloadFile(container, sourceFile, saveFile, appendFilePath, processCallback, maxTries, currentTries);
         }
 
         /// <summary>
         /// Download a file from Azure server async
         /// </summary>
         /// <param name="container">The name of the CloudBlobContainer</param>
-        /// <param name="sourceLocation">The blob name</param>
-        /// <param name="saveLocation">The path to the file we want to delete</param>
+        /// <param name="sourceFile">The blob name</param>
+        /// <param name="saveFile">The path to the file we want to delete</param>
+        /// <param name="appendFilePath">append the source path onto the save file path</param>
         /// <param name="processCallback">The progress callback</param>
         /// <param name="maxTries">Maximum numbers of tried before failing</param>
         /// <param name="currentTries">current number of tried for the download</param>
         /// <returns></returns>
-        public static async Task<bool> DownloadFile(BlobContainerClient container, string sourceLocation, string saveLocation, Action<ProgressRecorder> processCallback = null, int maxTries = 3, int currentTries = 0)
+        public static async Task<bool> DownloadFile(BlobContainerClient container, string sourceFile, string saveFile, bool appendFilePath = true, Action<ProgressRecorder> processCallback = null, int maxTries = 3, int currentTries = 0)
         {
             if (currentTries > maxTries)
             {
@@ -196,23 +197,32 @@ namespace Atkins.AzureHelpers
             }
             try
             {
-                BlobBaseClient cloudBlob = await GetCloudBlobAsync(container, sourceLocation);
+                BlobBaseClient cloudBlob = await GetCloudBlobAsync(container, sourceFile);
                 await new WaitForUpdate();
                 Debug.Log("Container :" + cloudBlob);
 
                 // Download a blob to your file system
-                string localPath = Path.Combine(saveLocation, sourceLocation);
+                string localPath = "";
+                if (appendFilePath)
+                {
+                    localPath = Path.Combine(saveFile, sourceFile);
+                }
+                else
+                {
+                    localPath = saveFile;
+                }
 
                 if (File.Exists(localPath))
                 {
                     await AsyncFile.DeleteAsync(localPath);
                 }
 
-                Debug.Log($"Downloading file {sourceLocation} to {localPath}");
+                Debug.Log($"Downloading file {sourceFile} to {localPath}");
 
-                using (FileStream outputFile = File.OpenWrite(localPath))
+                Directory.CreateDirectory(Path.GetDirectoryName(localPath) ?? string.Empty); //ensure the directory exists
+                using (FileStream outputFile = new FileStream(localPath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
                 {
-                    ProgressRecorder progressHandler = new ProgressRecorder(Path.GetFileNameWithoutExtension(sourceLocation), await CheckFileSize(container, sourceLocation))
+                    ProgressRecorder progressHandler = new ProgressRecorder(Path.GetFileNameWithoutExtension(sourceFile), await CheckFileSize(container, sourceFile))
                     {
                         updateCallback = processCallback
                     };
@@ -222,7 +232,6 @@ namespace Atkins.AzureHelpers
                     int bytesRead;
                     int totalBytesDownloaded = 0;
 
-                    Directory.CreateDirectory(Path.GetDirectoryName(localPath) ?? string.Empty); //ensure the directory exists
                     BlobDownloadInfo blobToDownload = (await cloudBlob.DownloadAsync()).Value;
                     
                     while((bytesRead = await blobToDownload.Content.ReadAsync(downloadBuffer,0,downloadBuffer.Length))!=0)
@@ -233,14 +242,14 @@ namespace Atkins.AzureHelpers
                     }
                 }
                 
-                Debug.Log($"Download finished {sourceLocation} to {localPath}");
+                Debug.Log($"Download finished {sourceFile} to {localPath}");
                 return true;
             }
             catch (Exception e)
             {
                 Debug.LogError(e);
                 currentTries ++;
-                return await DownloadFile(container, sourceLocation, saveLocation, processCallback,maxTries, currentTries);
+                return await DownloadFile(container, sourceFile, saveFile, appendFilePath, processCallback,maxTries, currentTries);
             }
             return false;
         }
@@ -291,7 +300,7 @@ namespace Atkins.AzureHelpers
         {
             try
             {
-                BlobBaseClient cloudBlob = await GetCloudBlobAsync(container, fileName);
+                BlobBaseClient cloudBlob = container.GetBlobClient(fileName);
                 return await cloudBlob.GetPropertiesAsync();
             }
             catch (Exception e)
